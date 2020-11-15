000bb55a00708de5457efd87d1c17420634107a1
Gustavo R. Silva <gustavors071998@gmail.com>
2020-11-15 15:12:57 -0300

    login and register

--- frontend/src/components/template/Login.jsx
+++ frontend/src/components/template/Login.jsx
@@ -1,43 +1,29 @@
 import React from 'react';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-import Login from "./login.component";
-import SignUp from "./signup";
-
-import './Login.css';
-
+import { Container, Row, Col } from 'reactstrap';
 
 function Loginx() {
   return (
-  <Router>
-    <div className="Loginc">
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand">Team Brabus</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/login"}>Entrar</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Registrar</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <Container className="container">
+      <Row>
+        <Col className="registrar">
+          <form>
+            <h3>Login</h3>
+            <div className="form-group">
+              <label>Email</label>
+              <input type="email" className="form-control" />
+            </div>
 
-      <div className="outer">
-        <div className="inner">
-          <Switch>
-            <Route exact path='/login' component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-          </Switch>
-        </div>
-      </div>
-    </div>
-    </Router>
+            <div className="form-group">
+              <label>Senha</label>
+              <input type="CPF" className="form-control" />
+            </div>
+
+            <button type="submit" className="btn btn-dark btn-lg btn-block">Entrar</button>
+          </form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- frontend/src/components/template/signup.jsx
+++ frontend/src/components/template/signup.jsx
@@ -1,34 +1,40 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from 'reactstrap';
+import './signup.css'
 
 export default class SignUp extends Component {
     render() {
         return (
-            <form>
-                <h3>Registrar</h3>
+            <Container className="container">
+                <Row>
+                    <Col className="registrar">
+                        <form>
+                            <h3>Registrar</h3>
+                            <div className="form-group">
+                                <label>Nome</label>
+                                <input type="text" className="form-control" />
+                            </div>
 
-                <div className="form-group">
-                    <label>Nome</label>
-                    <input type="text" className="form-control"/>
-                </div>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input type="email" className="form-control" />
+                            </div>
 
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" />
-                </div>
+                            <div className="form-group">
+                                <label>CPF</label>
+                                <input type="CPF" className="form-control" />
+                            </div>
 
-                <div className="form-group">
-                    <label>CPF</label>
-                    <input type="CPF" className="form-control" />
-                </div>
+                            <div className="form-group">
+                                <label>Senha</label>
+                                <input type="password" className="form-control" />
+                            </div>
 
-                <div className="form-group">
-                    <label>Senha</label>
-                    <input type="password" className="form-control" />
-                </div>
-
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Registrar</button>
-                
-            </form>
+                            <button type="submit" className="btn btn-dark btn-lg btn-block">Registrar</button>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
